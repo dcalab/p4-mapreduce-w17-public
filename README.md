1334af73bb650a1ca8b6313260a10556b2f6a302
Isaac Bowen <irbowen@umich.edu>
2017-03-06 20:14:12 -0500

    P4 Starter files first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+EECS 485 Project 4: Map Reduce
+==============================
+
+
+## Quick start
+Check out code:
+```
+git clone git@github.com:eecs485staff/p4-mapreduce-w17.git
+```
+
+Install Python requirements.
+```
+cd p4-mapreduce-w17/
+source {your path here}/venv/bin/activate
+pip install -r reqs.txt
+```
+
+Check out the spec under the spec/ folder.
---
+++ start.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#Feel free to change this
+PORT=6000
+
+# Start up the master on port 6000
+# The & means to run it in the background
+python3 master.py $PORT &
+
+# Sleep for 2 seconds for the master to start up
+sleep 2
+
+# Start two workers, telling them to talk to the master on port 6000
+python3 worker.py $PORT (($PORT + 1)) &
+sleep 1
+python3 worker.py $PORT (($PORT + 2)) &
+sleep 1
+
+
